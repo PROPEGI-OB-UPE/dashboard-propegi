b0b4471d4c921a30ccade323d17678e57303c090
Izaac Moraes <izaac.moraes@hotmail.com>
2026-06-01 18:30:23 -0300

    Corrigindo barras de diretorio para compatibilidade com Linux

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -5,31 +5,31 @@ import os
 # Define a raiz baseada onde está o utils/data_loader.py (uma pasta acima)
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-def carregar_csv(caminho_relativo):
-    caminho_completo = os.path.join(BASE_DIR, caminho_relativo)
+def carregar_csv(*caminho_partes):
+    caminho_completo = os.path.join(BASE_DIR, *caminho_partes)
     if os.path.exists(caminho_completo):
         return pd.read_csv(caminho_completo)
     return pd.DataFrame()
 
 @st.cache_data
 def carregar_dados_docentes():
-    return carregar_csv(r"data\processed\pesquisa\DOCENTES-ATIVOS\docentes_ativos.csv")
+    return carregar_csv("data", "processed", "pesquisa", "DOCENTES-ATIVOS", "docentes_ativos.csv")
 
 @st.cache_data
 def carregar_dados_grupos_pesquisa():
-    return carregar_csv(r"data\processed\pesquisa\GRUPO-PESQUISA\grupos_pesquisa.csv")
+    return carregar_csv("data", "processed", "pesquisa", "GRUPO-PESQUISA", "grupos_pesquisa.csv")
 
 @st.cache_data
 def carregar_dados_apq():
-    return carregar_csv(r"data\processed\pesquisa\PLANILHAS-APQ\prestacao_contas_apq.csv")
+    return carregar_csv("data", "processed", "pesquisa", "PLANILHAS-APQ", "prestacao_contas_apq.csv")
 
 @st.cache_data
 def carregar_dados_icti():
-    return carregar_csv(r"data\processed\pesquisa\PLANILHAS-ICTI\monitoramento_icti.csv")
+    return carregar_csv("data", "processed", "pesquisa", "PLANILHAS-ICTI", "monitoramento_icti.csv")
 
 @st.cache_data
 def carregar_dados_bolsistas():
-    pasta = os.path.join(BASE_DIR, r"data\processed\pesquisa\BOLSAS-PQ-DT")
+    pasta = os.path.join(BASE_DIR, "data", "processed", "pesquisa", "BOLSAS-PQ-DT")
     if not os.path.exists(pasta):
         return pd.DataFrame()
     arquivos = [os.path.join(pasta, f) for f in os.listdir(pasta) if f.endswith('.csv')]
